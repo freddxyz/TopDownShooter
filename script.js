e49779acb85471363f162555847917f2a224d397
FradZGenius <42040138+FradZGenius@users.noreply.github.com>
2021-05-17 21:24:14 -0400

    amongus

--- script.js
+++ script.js
@@ -4,22 +4,39 @@ class Vector2 {
     this.y = y;
   }
   get magnitude(){
-    
+    return Math.sqrt(this.x * this.x + this.y * this.y);
   }
   add(vec2){
-    return new Vector2(this.x+vec2.x, this.y+vec2.y)
+    return new Vector2(this.x+vec2.x, this.y+vec2.y);
   }
   multiplyScalar(scalar){
-    return new Vector2(this.x*scalar, this.y*scalar)
+    return new Vector2(this.x*scalar, this.y*scalar);
+  }
+  divideScalar(scalar){
+    return this.multiplyScalar(1/scalar);
   }
   normalize(){
+    return this.divideScalar(this.magnitude || 1);
+  }
+}
+
+class Object{
+  constructor(){
+    this.position = new Vector2(0,0);
+    this.size = new Vector2(1,1);
+    this.formFactor = "Rectangle";
+    this.velocity = new Vector2(0,0);
+    this.rotation = 0;
+  }
+  update(){
 
   }
 }
 
+function setup(){
 
-class Object{
-  constructor(){
+}
 
-  }
+function draw(){
+  
 }
