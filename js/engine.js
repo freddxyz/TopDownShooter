869ea5ddb5460aff33ad3e6de371090916735f91
FradZGenius <42040138+FradZGenius@users.noreply.github.com>
2021-05-23 02:48:46 -0400

    added bullets, player movement, formfactors, base object, parent/children system, and a few other things i can't remember

--- js/engine.js
+++ js/engine.js
@@ -1,3 +1,7 @@
+function getDelta(){
+	return deltaTime/1000;
+}
+
 class Vector2 {
   	constructor(x,y){
     	this.x = x;
@@ -5,11 +9,11 @@ class Vector2 {
   	}
   	get magnitude(){
     	return Math.sqrt(this.x * this.x + this.y * this.y);
- 	}
+ 		}
   	add(vec2){
     	return new Vector2(this.x+vec2.x, this.y+vec2.y);
   	}
-	sub(vec2){
+		sub(vec2){
     	return new Vector2(this.x-vec2.x, this.y-vec2.y);
   	}  
   	multiplyScalar(scalar){
@@ -23,16 +27,31 @@ class Vector2 {
 	}
 }
 
+class Color {
+	constructor(r,g,b,a=255){
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.a = a;
+	}
+	setColor(){
+		fill(this.r,this.g,this.b,this.a)
+	}
+}
+
 class InputManager {
 	constructor(){
+		this.keysDown = {bruh:"moment"};
 		this.keysDown = {};
+		this.mouseButtonsDown = {};
 	}
 	connect(){
-		document.addEventListener('keydown', this.onKeyDown)
-		document.addEventListener('keyup', this.onKeyUp);
+		document.addEventListener('keydown', evnt => this.onKeyDown(evnt));
+		document.addEventListener('keyup', evnt => this.onKeyUp(evnt));
+		document.addEventListener('mousedown', evnt => this.onMouseButtonDown(evnt));
+		document.addEventListener('mouseup', evnt => this.onMouseButtonUp(evnt));
 	}
 	onKeyDown(evnt){
-		console.log(this.keysDown)
 		this.keysDown[evnt.keyCode] = true;
 	}
 	onKeyUp(evnt){
@@ -43,35 +62,183 @@ class InputManager {
 	getKeyDown(code){
 		if(this.keysDown[code]){
 			return true;
-		}else{
-			return false;
-		} 
+		}
+		return false;
+	}
+	onMouseButtonDown(evnt){
+		this.mouseButtonsDown[evnt.button] = true;
+	}
+	onMouseButtonUp(evnt){
+		this.mouseButtonsDown[evnt.button] = false;
+	}
+	getMouseButtonDown(btn){
+		if(this.mouseButtonsDown[btn]){
+			return true;
+		}
+		return false;
+	}
+	get mousePos(){
+		return new Vector2(mouseX, mouseY);
 	}
 }
 
-class GameObject {
+var input = new InputManager();
+input.connect();
+
+class BaseObject {
 	constructor(){
 		this.position = new Vector2(0,0);
-    	this.size = new Vector2(1,1);
-    	this.formFactor = "Rectangle";
-		this.velocity = new Vector2(100,100);
 		this.rotation = 0;
+		this.pivot = new Vector2(0,0)
+		this.children = [];
+		this.parent;
+	}
+	setParent(parent){
+		this.parent = parent;
+		this.parent.updateChildren(this);
+	}
+	removeChild(child){
+		var i = this.children.indexOf(child);
+		this.children.splice(i,1);
+		
+	}
+	updateChildren(child){
+		if(!this.children[child]){
+			this.children.push(child);
+		}
+	}
+	getGlobalPosition(){
+		return this.position.add(this.parent.position);
+	}
+}
+
+const ROOT = new BaseObject();
+
+class GameObject extends BaseObject {
+	constructor(){
+		super();
+		this.color = new Color(255,255,255);
+    	this.size = new Vector2(1,1);
+    	this.formFactor = "RECT";
+		this.velocity = new Vector2(0,0);
 		this.friction = 2;
 	}
+	
 	update(){
-		rect(this.position.x,this.position.y,this.size.x,this.size.y);
 		let dt = deltaTime/1000;
 		let ratio = 1/(1+(dt*this.friction));
 		this.velocity =  this.velocity.multiplyScalar(ratio)
 		this.position = this.position.add(this.velocity.multiplyScalar(dt));
+		push();
+		this.color.setColor();
+		
+		if(this.parent){
+			translate(this.parent.position.x, this.parent.position.y);
+			rotate(this.parent.rotation);
+		}
+		translate(this.position.x,this.position.y);
+		rotate(this.rotation);
+		switch(this.formFactor){
+			case "RECT":
+				rect(this.pivot.x,this.pivot.y,this.size.x,this.size.y);
+				break;
+			case "ELLIPSE":
+				ellipse(this.pivot.x,this.pivot.y,this.size.x,this.size.y);
+				break;
+		}
+		
+		pop();
 	}
-	addforce(force){
+	addForce(force){
 		this.velocity = this.velocity.add(force)
+	}
+}
+
+class Bullet extends GameObject {
+	constructor(timeToLive){
+		super();
+		this.timeToLive = timeToLive;
+		this.ticker = 0;
+		this.setParent(ROOT);
+	}
+	update(){
+		super.update();
+		this.ticker += getDelta();
+		if(this.ticker >= this.timeToLive){
+			ROOT.removeChild(this);
+		}
 	}
 }
 
 class Player extends GameObject {
 	constructor(){
-		super()
+		super();
+		this.speed = 10;
+
+		this.fireRate = .1;
+		this.fireTicker = 0;
+		this.canFire = true;
+
+		this.knockBack = .1;		
+		this.bulletSpeed = 2000;
+		this.bulletLife = .5;
+
+		this.muzzle;
 	}
+	update(){
+		super.update();
+		this.fireTicker += getDelta();
+		if(this.fireTicker > this.fireRate) this.canFire = true;
+		super.update();
+		this.rotation = atan2(mouseY - this.position.y, mouseX - this.position.x)
+		if(input.getKeyDown(87)){
+			//w
+			this.velocity.y -= this.speed;
+		}if (input.getKeyDown(83)){
+			//s
+			this.velocity.y += this.speed;
+		}if (input.getKeyDown(65)){
+			//a
+			this.velocity.x -= this.speed;
+		}if (input.getKeyDown(68)){
+			//d
+			this.velocity.x += this.speed;
+		}
+		if(input.getMouseButtonDown(0)){
+			this.fire();
+		}
+	}
+	fire(){
+		if(this.canFire){
+			this.fireTicker = 0;
+			this.canFire = false;
+		}else{
+			return;
+		}
+		let proj = new Bullet(.5);
+		proj.position = this.position;
+		
+		proj.size.x = 40;
+		proj.size.y = 15;
+		proj.rotation = this.rotation;
+		proj.pivot = this.muzzle.position;
+		proj.formFactor = "ELLIPSE";
+		proj.color = new Color(255,0,0,255)
+		if(this.muzzle){
+			proj.velocity = new Vector2(cos(proj.rotation), sin(proj.rotation)).multiplyScalar(this.bulletSpeed)//this.muzzle.getGlobalPosition().sub(input.mousePos).normalize().multiplyScalar(-1000);
+			this.addForce(proj.velocity.multiplyScalar(-this.knockBack))
+		}
+		proj.friction = 0;
+	}
+}
+
+function drawChildren(item){
+	item.children.forEach(child => {
+		child.update();
+		drawChildren(child);
+	});
+}
+
+function drawObjects(){
+	drawChildren(ROOT);
 }
--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
-let obj = new GameObject();
-let inputManager = new InputManager();
-inputManager.connect();
+let plr = new Player();
+plr.setParent(ROOT);
+let gun = new GameObject();
+let helmet = new GameObject();
+helmet.setParent(plr);
+gun.setParent(plr);
+//let inputManager = new InputManager();
+//inputManager.connect();
 
 function setup(){
+	rectMode(CENTER);
+	noStroke();
 	createCanvas(window.innerWidth, window.innerHeight);
+	
+	plr.color = new Color(255,255,0,255);
+	plr.size = new Vector2(100,100);
+	plr.formFactor = "ELLIPSE";
+
+	plr.friction = 7;
+	plr.speed = 50;
+
+	plr.knockBack = .05;
+	plr.fireRate = .05;
+	plr.bulletLife = .5;
+	plr.bulletSpeed = 2000;
+
+	plr.muzzle = gun;
+
+	gun.size = new Vector2(80,20);
+	gun.position.x = 50;
+	gun.position.y = 0;
+	gun.color = new Color(128,128,128);
+	gun.formFactor = "RECT"
+
+	helmet.size = new Vector2(50,50);
+	helmet.color = new Color(200,200,200);
+	helmet.formFactor = "ELLIPSE";
 }
 
 function draw() {
-	//obj.velocity = new Vector2(.5,0)
+	//plr.velocity = new Vector2(.5,0)
 	background(0);
-	obj.update();
-	obj.size = new Vector2(100,100)
+	drawObjects();
+	//plr.addForce(new Vector2(1,1))
 }
 
 function windowResized() {
